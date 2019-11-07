8a9020cd938d48802aa427f1f6b3fb194e270248
FacuVi <57237853+FacuVi@users.noreply.github.com>
2019-11-07 20:49:56 -0300

    Handles
    agregue una carpeta de handlesEditor y un handlesTransform, que iria mas conectado con otra cosa del army

---
+++ APMOTORES TP/Assets/HandlesTransform.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandlesTransform : MonoBehaviour
+{
+    public Transform soldier0;
+    public Transform soldier1;
+    public Transform soldierGreen;
+    public Transform soldierRed;
+    // esto lo tendria que tener army y pasar el transform de los soldiers
+}
